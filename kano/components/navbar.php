ad248b815b56edee69045671d817b8ac95cc8f08
dapla365 <plazadiazdavid@gmail.com>
2024-04-02 15:53:34 +0200

    Paginas - Peluqueros - Imagen

--- kano/components/navbar.php
+++ kano/components/navbar.php
@@ -1,9 +1,9 @@
 <header>
 <div class="centro">
-<a href=""><i class="bi bi-house-door-fill"></i><h2>Inicio</h2></a>
-<a href=""><i class="bi bi-scissors"></i><h2>Peluqueros</h2></a>
-<a href=""><i class="bi bi-calendar2-week-fill"></i><h2>Disponibles</h2></a>
-<a href=""><i class="bi bi-calendar-check-fill"></i><h2>Reservas</h2></a>
-<a href=""><i class="bi bi-person-fill"></i><h2>Perfil</h2></a>
+    <a href="index.php"><i class="bi bi-house-door-fill"></i><h2>Inicio</h2></a>
+    <a href="peluqueros.php"><i class="bi bi-scissors"></i><h2>Peluqueros</h2></a>
+    <a href="disponibles.php"><i class="bi bi-calendar2-week-fill"></i><h2>Disponibles</h2></a>
+    <a href="reservas.php"><i class="bi bi-calendar-check-fill"></i><h2>Reservas</h2></a>
+    <a href="perfil.php"><i class="bi bi-person-fill"></i><h2>Perfil</h2></a>
 </div>
 </header>
---
+++ kano/peluqueros.php
@@ -0,0 +1,12 @@
+<?php include "components/header.php" ?>
+<?php include "components/navbar.php" ?>
+
+<div class="peluqueros">
+    <div class="centro">
+        <div class="peluquero"><img src="images/favicon.png" alt="foto">
+            <h2>Nombre</h2>
+        </div>
+    </div>
+</div>
+
+<?php include "components/footer.php" ?>
