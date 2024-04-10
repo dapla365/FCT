c3f972dd0a485495fc8a0281e3184fc9efa58c33
dapla365 <davidplaza03@iesamachado.org>
2024-04-10 17:49:08 +0200

    perfil css

--- kano/perfil.php
+++ kano/perfil.php
@@ -1,40 +1,26 @@
 <?php include "components/header.php" ?>
 <?php include "components/navbar.php" ?>
-
-<section class="seccion-perfil-usuario">
-    <div class="perfil-usuario-header">
-        <div class="perfil-usuario-portada">
-            <div class="perfil-usuario-avatar">
-                <img src="<?php echo $user_foto;?>" alt="img-avatar">
-                <button type="button" class="boton-avatar">
-                    <i class="bi bi-image-fill"></i>
-                </button>
+<div class="centro">
+    <div class="container">
+        <div class="left">
+            <img src="<?php echo $user_foto; ?>" alt="img-avatar">
+            <h2><?php echo "$user_nombre $user_apellidos"; ?></h2>
+            <h3><?php echo ucwords(mb_strtolower($user_rolname)); ?></h3>
+        </div>
+        <div class="right">
+            <div class="datos">
+                <p class="info">Correo electrónico</p> <span><?php echo $user_correo; ?></span>
             </div>
-            <button type="button" class="boton-portada">
-                <i class="bi bi-image-fill"></i> Cambiar fondo
-            </button>
+            <div class="datos">
+                <p class="info">Nombre</p> <span><?php echo $user_nombre; ?></span>
+            </div>
+            <div class="datos">
+                <p class="info">Apellidos</p> <span><?php echo ucwords($user_apellidos); ?></span>
+            </div>
+            <div class="datos">
+                <p class="info">Cargo</p> <span><?php echo ucwords(mb_strtolower($user_rolname)); ?></span>
+            </div>
         </div>
     </div>
-    <div class="perfil-usuario-body">
-        <div class="perfil-usuario-bio">
-            <h3 class="titulo"><?php echo "$user_nombre $user_apellidos";?></h3>
-            <p class="texto"><?php echo ucfirst(mb_strtolower($user_rolname));?> </p>
-        </div>
-        <div class="perfil-usuario-footer">
-            <ul class="lista-datos">
-                <li><i class="icono"></i> Correo:</li>
-                <li><i class="icono"></i> Nombre:</li>
-                <li><i class="icono"></i> Apellidos:</li>
-                <li><i class="icono"></i> Cargo:</li>
-            </ul>
-            <ul class="datos-right">
-                <li><i class="icono"></i><?php echo $user_correo;?></li>
-                <li><i class="icono"></i><?php echo $user_nombre;?></li>
-                <li><i class="icono"></i><?php echo $user_apellidos;?></li>
-                <li><i class="icono"></i><?php echo ucfirst(mb_strtolower($user_rolname)); ?></li>
-            </ul>
-        </div>
-    </div>
-</section>
-
+</div>
 <?php include "components/footer.php" ?>
