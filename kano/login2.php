2e3b5a051410383e46635bd503017faaec89eef1
dapla365 <plazadiazdavid@gmail.com>
2024-05-09 15:54:59 +0200

    login google api

--- kano/login2.php
+++ kano/login2.php
@@ -1,71 +1,36 @@
+
 <?php
-
-session_start();
-
-require_once 'google-api-php-client/vendor/autoload.php';
+include 'components/header.php';
 include 'components/secret.json';
+require_once 'google-api-php-client/vendor/autoload.php';
 
-use Google\Client;
-
-# Determines where the API server redirects the user after the user completes the authorization flow
-# This value must exactly match one of the authorized redirect URIs for the OAuth 2.0 client, which you configured in your client’s API Console Credentials page.
-$redirectUrl = 'http://dapla.thsite.top/kano/';
-
-# Create an configure client
-$client = new Client();
-$client->setAuthConfig('components/secret.json');
-$client->setRedirectUri($redirectUrl);
-$client->addScope('https://www.googleapis.com/auth/youtube');
-
-
-# === SCENARIO 1: PREPARE FOR AUTHORIZATION ===
-if(!isset($_GET['code']) && empty($_SESSION['google_oauth_token'])) {
-    $_SESSION['code_verifier'] = $client->getOAuth2Service()->generateCodeVerifier();
-
-    # Get the URL to Google’s OAuth server to initiate the authentication and authorization process
-    $authUrl = $client->createAuthUrl();
-
-    $connected = false;
-}
-
-
-# === SCENARIO 2: COMPLETE AUTHORIZATION ===
-# If we have an authorization code, handle callback from Google to get and store access token
+// init configuration
+$clientID = $client_id; // your client id
+$clientSecret = $client_secret; // your client secret
+$redirectUri = $redirect_uris;
+// create Client Request to access Google API 
+$client = new Google_Client();
+$client->setClientId($clientID);
+$client->setClientSecret($clientSecret);
+$client->setRedirectUri($redirectUri);
+$client->addScope("email");
+$client->addScope("profile");
+ 
+// authenticate code from Google OAuth Flow 
 if (isset($_GET['code'])) {
-    # Exchange the authorization code for an access token
-    $token = $client->fetchAccessTokenWithAuthCode($_GET['code'], $_SESSION['code_verifier']);
-    $client->setAccessToken($token);
-    $_SESSION['google_oauth_token'] = $token;
-    header('Location: ' . $redirectUrl);
-}
-
-
-# === SCENARIO 3: ALREADY AUTHORIZED ===
-# If we’ve previously been authorized, we’ll have an access token in the session
-if (!empty($_SESSION['google_oauth_token'])) {
-    $client->setAccessToken($_SESSION['google_oauth_token']);
-    if ($client->isAccessTokenExpired()) {
-        $_SESSION['google_oauth_token'] = null;
-        $connected = false;
-    }
-    $connected = true;
-}
-
-# === SCENARIO 4: TERMINATE AUTHORIZATION ===
-if(isset($_GET['disconnect'])) {
-    $_SESSION['google_oauth_token'] = null;
-    $_SESSION['code_verifier'] = null;
-    header('Location: ' . $redirectUrl);
+  $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+  $client->setAccessToken($token['access_token']);
+  
+  echo $client;
+  // get profile info 
+  /*$google_oauth = new Google_Service_Oauth2($client);
+  $google_account_info = $google_oauth->userinfo->get();
+  $email =  $google_account_info->email;
+  $name =  $google_account_info->name;*/
+ 
+  // now you can use this profile info to create account in your website and make user logged in. 
+} else {
+  echo "<a href='".$client->createAuthUrl()."'>Google Login</a>";
 }
 ?>
 
-<h1>Demo</h1>
-<p>
-    <strong>Status:</strong>
-    <?php if($connected): ?>
-        Authorized. <a href='?disconnect'>Disconnect</a>
-    <?php else: ?>
-        Not authorized. 
-        <a href='<?php echo $authUrl; ?>'>Authorize with Google...</a>
-    <?php endif; ?>
-</p>
