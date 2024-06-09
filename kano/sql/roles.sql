f475c6e9a41d8caeb4037eb75f90d4278fc68840
dapla365 <48187230+dapla365@users.noreply.github.com>
2024-06-09 15:49:54 +0200

    Update roles.sql

--- kano/sql/roles.sql
+++ kano/sql/roles.sql
@@ -4,7 +4,7 @@ CREATE TABLE roles (
     nivel INT NOT NULL DEFAULT 0
 );
 
-INSERT INTO roles VALUES (0, 'USUARIO', 0);
-INSERT INTO roles VALUES (1, 'PELUQUERO', 5);
-INSERT INTO roles VALUES (2, 'JEFE-PELUQUERO', 10);
-INSERT INTO roles VALUES (3, 'JEFE', 10);
+INSERT INTO roles(nombre, nivel) VALUES ('USUARIO', 0);
+INSERT INTO roles(nombre, nivel) VALUES ('PELUQUERO', 5);
+INSERT INTO roles(nombre, nivel) VALUES ('JEFE-PELUQUERO', 10);
+INSERT INTO roles(nombre, nivel) VALUES ('JEFE', 10);
